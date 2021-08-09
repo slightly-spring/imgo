7dcaaa6fb5c9a2a1cbbd55f06fbd84e132fceb1a
zwan <2zwan@naver.com>
2021-08-09 16:21:03 +0900

    spring init

---
+++ src/test/java/slightlyspring/imgo/ImgoApplicationTests.java
@@ -0,0 +1,13 @@
+package slightlyspring.imgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
