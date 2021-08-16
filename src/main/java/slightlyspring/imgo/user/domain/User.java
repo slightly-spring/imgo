656ed605f8f466fb7af3c5e0a7a3f5b657bbf794
Hyun-juhee <juhe1998@naver.com>
2021-08-16 16:40:39 +0900

    feat(user/domain) : User 관련 엔티티 생성 및 매핑

--- src/main/java/slightlyspring/imgo/user/domain/User.java
+++ src/main/java/slightlyspring/imgo/user/domain/User.java
@@ -1,10 +1,12 @@
 package slightlyspring.imgo.user.domain;
 
-import lombok.Generated;
 import lombok.Getter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -12,10 +14,8 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id @GeneratedValue
+    @Column(name = "user_id")
     private Long id;
-
-    @OneToOne(mappedBy = "UserAccount")
-    private UserAccount userAccount;
 
     private String nickname;
 
@@ -27,8 +27,18 @@ public class User {
 
     private int maxContinuousDays;
 
-    private LocalDateTime createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 
-    private LocalDateTime modifiedAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedAt;
 
+    @OneToOne(mappedBy = "user")
+    private UserAccount userAccount;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserTilRecord> userTilRecords = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<UserNotice> userNotices = new ArrayList<>();
 }
