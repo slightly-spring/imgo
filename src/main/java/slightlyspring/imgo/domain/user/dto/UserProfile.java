e6c47e3c7ae252596b687073c8355d375899342e
zwan <2zwan@naver.com>
2021-08-29 23:40:57 +0900

    feat(profile): add dto (UserProfile, UserProfileDetail)

---
+++ src/main/java/slightlyspring/imgo/domain/user/dto/UserProfile.java
@@ -0,0 +1,14 @@
+package slightlyspring.imgo.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserProfile {
+
+    private Long id;
+
+    private String nickname;
+    private String profileImg;
+    private String profileDescription;
+}
