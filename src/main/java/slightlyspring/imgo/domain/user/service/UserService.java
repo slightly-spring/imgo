aea0d4c21c844670448abb2ba97b1212c7533104
zwan <2zwan@naver.com>
2021-09-01 16:14:56 +0900

    feat(profile): add getUserProfileDetail()

--- src/main/java/slightlyspring/imgo/domain/user/service/UserService.java
+++ src/main/java/slightlyspring/imgo/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import slightlyspring.imgo.domain.user.domain.User;
 import slightlyspring.imgo.domain.user.dto.UserProfile;
+import slightlyspring.imgo.domain.user.dto.UserProfileDetail;
 import slightlyspring.imgo.domain.user.repository.UserRepository;
 
 import java.util.Optional;
@@ -24,6 +25,12 @@ class UserService {
         UserProfile userProfile = modelMapper.map(user, UserProfile.class);
 
         return userProfile;
+    }
+
+    public UserProfileDetail getUserProfileDetail(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        UserProfileDetail userProfileDetail = modelMapper.map(user, UserProfileDetail.class);
+        return userProfileDetail;
     }
 
 }
