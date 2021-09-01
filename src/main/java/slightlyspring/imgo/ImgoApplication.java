9970112019f986ce38bfb7c67e50c78def890e3f
zwan <2zwan@naver.com>
2021-09-01 16:46:26 +0900

    feat(thymeleaf): add thymeleaf
    - add initial example

--- src/main/java/slightlyspring/imgo/ImgoApplication.java
+++ src/main/java/slightlyspring/imgo/ImgoApplication.java
@@ -2,21 +2,27 @@ package slightlyspring.imgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class ImgoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImgoApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
 
 }
