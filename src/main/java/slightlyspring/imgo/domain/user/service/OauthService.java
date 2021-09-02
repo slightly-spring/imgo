251ae7f1e84342ca31dce115f78e0438504f79bd
gurunelee <dlckdgk4858@gmail.com>
2021-09-02 22:49:51 +0900

    feat(social login): add functions for Requesting the AccessToken AccessToken 을 받아오는 함수들

--- src/main/java/slightlyspring/imgo/domain/user/service/OauthService.java
+++ src/main/java/slightlyspring/imgo/domain/user/service/OauthService.java
@@ -11,16 +11,18 @@ import slightlyspring.imgo.domain.user.service.social.GoogleOauth;
 @RequiredArgsConstructor // 뭐더라
 public class OauthService {
 
-  /**
-   * 여러 소셜 로그인으로 확장 시
-   * FacebookOauth, KakaoOauth 등의 클래스를 추가하여 구현
-   */
+  // 여러 소셜 로그인으로 확장 시 FacebookOauth, KakaoOauth 등의 클래스를 추가하여 구현
   private final GoogleOauth googleOauth;
 
   private final HttpServletResponse response;
 
+  /**
+   * Social login API 서버에 리퀘스트를 보내는 주체
+   * @param authType Social Service의 종류 (google, facebook, kakao ...)
+   */
   public void request(AuthType authType) {
     String redirectURL;
+    // facebook, kakao 등 추가 가능
     switch (authType) {
       case GOOGLE: {
         redirectURL = googleOauth.getOauthRedirectURL();
@@ -34,6 +36,23 @@ public class OauthService {
       response.sendRedirect(redirectURL);
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  /**
+   * 받은 code로 AccessToken을 받아오는 request
+   * @param authType
+   * @param code
+   * @return accessToken을 String형태로 반환
+   */
+  public String requestAccessToken(AuthType authType, String code) {
+    switch (authType) {
+      case GOOGLE: {
+        return googleOauth.requestAccessToken(code);
+      }
+      default: {
+        throw new IllegalArgumentException("등록되지 않은 소셜 로그인 형식입니다");
+      }
     }
   }
 }
--- src/main/java/slightlyspring/imgo/domain/user/service/social/SocialOauth.java
+++ src/main/java/slightlyspring/imgo/domain/user/service/social/SocialOauth.java
@@ -6,4 +6,12 @@ public interface SocialOauth {
    * 사용자로부터 로그인 요청을 받아 Social Login Server 인증용 code 요청
    */
   public String getOauthRedirectURL();
+
+  /**
+   * API 서버로부터 받은 code를 활용해 사용자 인증정보 요청
+   *
+   * @param code API 서버에서 받아온 code
+   * @return API 서버로부터 응답받은 Json형태의 결과를 string으로 반환
+   */
+  public String requestAccessToken(String code);
 }
