c7c46f2c17a34b99f6086f073565a8e3d87fe0ee
gurunelee <dlckdgk4858@gmail.com>
2021-09-03 10:38:32 +0900

    chore: temp test code (WIP)

---
+++ src/test/java/slightlyspring/imgo/domain/user/service/social/GoogleOauthTest.java
@@ -0,0 +1,31 @@
+package slightlyspring.imgo.domain.user.service.social;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+//어떻게 작성해야 하지
+class GoogleOauthTest {
+
+  GoogleOauth go = new GoogleOauth();
+
+  @Test
+  void getOauthRedirectURL() {
+    //given
+
+    //when
+
+    //then
+  }
+
+  @Test
+  void requestAccessToken() {
+    //given
+
+    //when
+
+    //then
+  }
+}
