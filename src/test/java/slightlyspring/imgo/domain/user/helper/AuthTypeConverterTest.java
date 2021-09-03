b01560f252f0282fc2f01c232e53a0add9855e55
gurunelee <dlckdgk4858@gmail.com>
2021-09-03 11:20:29 +0900

    fix: add annotation to exiting test @SpringbootTest / @ActiveProfiles("test")

--- src/test/java/slightlyspring/imgo/domain/user/helper/AuthTypeConverterTest.java
+++ src/test/java/slightlyspring/imgo/domain/user/helper/AuthTypeConverterTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class AuthTypeConverterTest {
 
   @Test
