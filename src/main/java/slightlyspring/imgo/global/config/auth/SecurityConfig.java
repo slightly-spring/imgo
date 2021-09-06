078bfa0dca2b6bc976fd9ee3d80dc131d6cfe600
clee <dlckdgk4858@gmail.com>
2021-09-06 23:35:05 +0900

    feat(oauth login):  set base-configuration for oauth2 login
    + 사용자정의 login page

--- src/main/java/slightlyspring/imgo/global/config/auth/SecurityConfig.java
+++ src/main/java/slightlyspring/imgo/global/config/auth/SecurityConfig.java
@@ -1,36 +1,36 @@
 package slightlyspring.imgo.global.config.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * SecurityConfig extends WebSecurityConfigurerAdapter
- * spring-security 설
+ * class SecurityConfig extends WebSecurityConfigurerAdapter
+ * spring-security 설정
  */
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity // spring security 설정 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final CustomOAuth2UserService customOAuth2UserService;
+//  private final CustomOAuth2UserService customOAuth2UserService;
+//  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+//  private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
-        .headers().frameOptions().disable()
+        // URL 별 권한 관리 설정 - antMatchers() 를 사용해 권한 별 설정이 가능. 현재는 그냥 모두 열어놓음.
+        .authorizeRequests()
+        .antMatchers("/oauth_login").permitAll()
+        .anyRequest().authenticated()
         .and()
-          // URL 별 권한 관리 설정 - antMatchers() 를 사용해 권한 별 설정이 가능. 현재는 그냥 모두 열어놓음.
-          .authorizeRequests()
-          .anyRequest().permitAll()// 수정 필요
-        .and()
-          .logout()
-            .logoutSuccessUrl("/")
-        .and()
-          .oauth2Login()
-            .userInfoEndpoint()
-              .userService(customOAuth2UserService); // 소셜 로그인 성공시, 후속조치를 진행할 인터페이스 구현체 등록
+        .oauth2Login()
+        .loginPage("/oauth_login");
+
+
   }
 
 }
