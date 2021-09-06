0f2e1b084d80925b6519a2ef9fb4addb2f3e53d1
clee <dlckdgk4858@gmail.com>
2021-09-07 01:37:40 +0900

    feat(oauth login):  사용자 정보 불러오기
    + 사용자정의 login page

--- src/main/java/slightlyspring/imgo/domain/user/controller/loginController.java
+++ src/main/java/slightlyspring/imgo/domain/user/controller/loginController.java
@@ -1,17 +1,28 @@
 package slightlyspring.imgo.domain.user.controller;
 
+import org.springframework.http.HttpHeaders;
+import java.util.Map;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 import slightlyspring.imgo.global.config.auth.dto.SessionUser;
 
 @RequiredArgsConstructor
 @Controller
 public class loginController {
 
-  private final HttpSession httpSession;
+//  private final HttpSession httpSession;
+  private OAuth2AuthorizedClientService authorizedClientService;
 
   /**
    * login
@@ -23,13 +34,38 @@ public class loginController {
   @GetMapping("/oauth_login")
   public String getLoginPage(Model model) {
     // HttpSession 에 저장되어있는 'user' 세션을 불러오기
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
-    if (user != null) {
-      //세션에 저장된 값이 있을때만, model 에 userName 으로 등록
-      //세션에 저장된 값이 없다면, 로그인 버튼만 보일 것
-      model.addAttribute("userName", user.getName());
-    }
+//    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//    if (user != null) {
+//      //세션에 저장된 값이 있을때만, model 에 userName 으로 등록
+//      //세션에 저장된 값이 없다면, 로그인 버튼만 보일 것
+//      model.addAttribute("userName", user.getName());
+//    }
     return "oauth_login";
   }
+
+  @GetMapping("/loginSuccess")
+  public String getLoginInfo(Model model, OAuth2AuthenticationToken authorizedToken) {
+    // 현재 userToken 에 해당하는 사용자 불러오기
+    OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+        authorizedToken.getAuthorizedClientRegistrationId(),
+        authorizedToken.getName()
+    );
+    // user info. endpoint 에 request 날려서 정보 받아오기
+    String userInfoEndpointUri = client.getClientRegistration()
+        .getProviderDetails().getUserInfoEndpoint().getUri();
+    if (!StringUtils.hasLength(userInfoEndpointUri)) {
+      RestTemplate restTemplate = new RestTemplate();
+      HttpHeaders headers = new HttpHeaders();
+      headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+          .getTokenValue());
+      HttpEntity entity = new HttpEntity("", headers);
+      ResponseEntity<Map> response = restTemplate
+          .exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+      Map userAttributes = response.getBody();
+      model.addAttribute("name", userAttributes.get("name"));
+    }
+    return "loginSuccess";
+  }
+
 
 }
