a3a8156bd26f4db0d40fd60dfa9812dd36cad0a8
zwan <2zwan@naver.com>
2021-09-06 21:44:45 +0900

    feat(profile): add isRivalActive()

--- src/main/java/slightlyspring/imgo/domain/user/dto/UserProfileDetail.java
+++ src/main/java/slightlyspring/imgo/domain/user/dto/UserProfileDetail.java
@@ -6,6 +6,8 @@ import slightlyspring.imgo.domain.user.domain.Rival;
 import slightlyspring.imgo.domain.user.domain.UserBadge;
 import slightlyspring.imgo.domain.user.domain.UserTilRecord;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,12 @@ public class UserProfileDetail {
     private List<UserBadge> userBadges = new ArrayList<>();
     private List<Rival> rivals = new ArrayList<>();
     private List<UserTilRecord> userTilRecords = new ArrayList<>();
+
+    public boolean isRivalActive(Rival rival) {
+        LocalDateTime activeTime = LocalDateTime.now().plusDays(-1);
+        Duration duration = Duration.between(activeTime, rival.getTarget().getLastWriteAt());
+        return duration.getSeconds() >= 0;
+    }
 
     public boolean isRecorded(int day) {
         return this.userTilRecords.stream().anyMatch(record -> record.getBaseDate().getDayOfMonth() == day);
