e641e195596c351d173ac19b2bb1e80a573c809e
zwan <2zwan@naver.com>
2021-09-14 15:43:45 +0900

    feat(til-write): add hibernate search (elasticsearch)

--- src/main/java/slightlyspring/imgo/domain/tag/service/TagService.java
+++ src/main/java/slightlyspring/imgo/domain/tag/service/TagService.java
@@ -1,4 +1,29 @@
 package slightlyspring.imgo.domain.tag.service;
 
+import org.hibernate.search.engine.search.query.SearchResult;
+import org.hibernate.search.mapper.orm.Search;
+import org.springframework.stereotype.Service;
+import slightlyspring.imgo.domain.tag.domain.Tag;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
 public class TagService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Tag> searchTags(String word) {
+        SearchResult<Tag> result = Search.session(entityManager)
+                .search(Tag.class)
+                .where(f -> f.match()
+                        .fields("name")
+                        .matching(word))
+                .fetch(10);
+        System.out.println("result = " + result);
+        return result.hits();
+    }
+
 }
--- src/test/java/slightlyspring/imgo/domain/tag/service/TagServiceTest.java
+++ src/test/java/slightlyspring/imgo/domain/tag/service/TagServiceTest.java
@@ -1,7 +1,65 @@
 package slightlyspring.imgo.domain.tag.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import slightlyspring.imgo.domain.tag.domain.Tag;
+import slightlyspring.imgo.domain.tag.repository.TagRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 class TagServiceTest {
 
+    @Autowired
+    TagService tagService;
+
+    @Autowired
+    TagRepository tagRepository;
+
+    @BeforeEach
+    void setup() {
+        Tag tag = Tag.builder()
+                .name("Spring")
+                .build();
+        Tag tag2 = Tag.builder()
+                .name("Spring Security")
+                .build();
+        Tag tag3 = Tag.builder()
+                .name("spring")
+                .build();
+        Tag tag4 = Tag.builder()
+                .name("Java")
+                .build();
+        Tag tag5 = Tag.builder()
+                .name("Javascript")
+                .build();
+
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(tag);
+        tagList.add(tag2);
+        tagList.add(tag3);
+        tagList.add(tag4);
+        tagList.add(tag5);
+
+        tagRepository.saveAll(tagList);
+    }
+
+    // FIXME tag 저장 시 elasticsearch에 즉시 인덱싱 안되는 문제 있음. test 전체로 돌렸을 때만 정상 실행 확인.
+    @Test
+    void tag_검색() {
+        // given
+
+        // when
+        List<Tag> hits = tagService.searchTags("spring");
+
+        // then
+        assertThat(hits.size()).isGreaterThan(0);
+    }
 }
