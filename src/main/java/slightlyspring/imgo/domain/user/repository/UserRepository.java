9a3fb00ad4c9ffe4c5b499e704d933fe52f17ec6
gurunelee <dlckdgk4858@gmail.com>
2021-09-14 17:08:08 +0900

    feat(oauth login): add findByNickname method to UserRepository

--- src/main/java/slightlyspring/imgo/domain/user/repository/UserRepository.java
+++ src/main/java/slightlyspring/imgo/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package slightlyspring.imgo.domain.user.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import slightlyspring.imgo.domain.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  List<User> findByNickname(String nickname);
 }
