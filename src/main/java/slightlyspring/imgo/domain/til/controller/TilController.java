fd2a0f3bbf6d22f85cc216fb7711b45f59afc6a7
zwan <2zwan@naver.com>
2021-09-15 02:02:47 +0900

    feat(til-write): til/write view layout
    - add modal lib
    - add editor lib
    - add tag lib

--- src/main/java/slightlyspring/imgo/domain/til/controller/TilController.java
+++ src/main/java/slightlyspring/imgo/domain/til/controller/TilController.java
@@ -1,4 +1,21 @@
 package slightlyspring.imgo.domain.til.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/til")
 public class TilController {
+
+    @GetMapping("/write")
+    public String write() {
+
+        // TODO 유저(+시리즈) 정보 가져오기
+
+        return "/til/write";
+    }
+
 }
