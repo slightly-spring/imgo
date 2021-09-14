b11fc126df2a017865f9fde481666f52befb7567
gurunelee <dlckdgk4858@gmail.com>
2021-09-14 17:20:36 +0900

    chore : change param of SessionUser Constructor

--- src/main/java/slightlyspring/imgo/domain/user/dto/SessionUser.java
+++ src/main/java/slightlyspring/imgo/domain/user/dto/SessionUser.java
@@ -3,7 +3,7 @@ package slightlyspring.imgo.domain.user.dto;
 import lombok.Getter;
 import slightlyspring.imgo.domain.user.domain.User;
 import slightlyspring.imgo.domain.user.domain.UserAccount;
-import slightlyspring.imgo.domain.user.helper.AuthType;
+import slightlyspring.imgo.domain.user.domain.AuthType;
 
 @Getter
 public class SessionUser {
@@ -12,9 +12,9 @@ public class SessionUser {
   private AuthType authType;
 
 
-  public SessionUser(UserAccount userAccount, User user) {
-    this.name = user.getNickname();
-    this.authId = userAccount.getAuthId();
-    this.authType = userAccount.getAuthType();
+  public SessionUser(OAuthAttributes attributes) {
+    this.name = attributes.getNickname();
+    this.authId = attributes.getAuthId();
+    this.authType = attributes.getAuthType();
   }
 }
