0c338bda40a6899482070fb1637df7bba5f1d1c3
gurunelee <dlckdgk4858@gmail.com>
2021-09-14 17:22:31 +0900

    feat(test) : add Tests

--- src/test/java/slightlyspring/imgo/domain/user/repository/UserAccountRepositoryTest.java
+++ src/test/java/slightlyspring/imgo/domain/user/repository/UserAccountRepositoryTest.java
@@ -3,31 +3,47 @@ package slightlyspring.imgo.domain.user.repository;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import groovy.util.logging.Slf4j;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import slightlyspring.imgo.domain.user.domain.AuthType;
 import slightlyspring.imgo.domain.user.domain.Role;
+import slightlyspring.imgo.domain.user.domain.User;
 import slightlyspring.imgo.domain.user.domain.UserAccount;
 
 @SpringBootTest
 @Transactional
-//@RequiredArgsConstructor
 class UserAccountRepositoryTest {
 
   @Autowired
   private UserAccountRepository userAccountRepository;
 
   @Test
+  void UserAccount_하나가_저장된다() {
+    //given
+    UserAccount userAccount = UserAccount.builder()
+        .authType(AuthType.GOOGLE)
+        .authId("testAuthId")
+        .role(Role.USER)
+        .build();
+    //when
+    userAccountRepository.save(userAccount);
+
+    //then
+    UserAccount findUserAccount = userAccountRepository.findByAuthId("testAuthId").get(0);
+    assertThat(findUserAccount.getAuthId()).isEqualTo("testAuthId");
+  }
+
+  @Test
   void TimeEntity_등록() {
     //given
 //    LocalDateTime now = LocalDateTime.of(2100,2,15,0,0,0);
     LocalDateTime now = LocalDateTime.now();
-    userAccountRepository.save(UserAccount.builder().name("userA").email("tmpEmail").picture("tmpPicture").role(
-        Role.USER).build());
+    userAccountRepository.save(UserAccount.builder().role(Role.USER).build());
 
     //when
     List<UserAccount> postsLists = userAccountRepository.findAll();
--- src/test/java/slightlyspring/imgo/domain/user/repository/UserRepositoryTest.java
+++ src/test/java/slightlyspring/imgo/domain/user/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package slightlyspring.imgo.domain.user.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,11 +14,32 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Transactional
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Test
+    void User가_저장된다() {
+        //given
+        User user = User.builder()
+            .nickname("nameA")
+            .profileImg("imageA")
+            .build();
+
+        //when
+        userRepository.save(user);
+
+        //then
+        List<User> findUsers = userRepository.findByNickname("nameA");
+        assertThat(findUsers.size()).isEqualTo(1);
+
+        User findUser = userRepository.findByNickname("nameA").get(0);
+        assertThat(findUser.getNickname()).isEqualTo("nameA");
+        assertThat(findUser.getProfileImg()).isEqualTo("imageA");
+        assertThat(findUser.getLastWriteAt()).isEqualTo(LocalDateTime.of(0, 0, 0, 0, 0, 0, 0));
+    }
 
 }
