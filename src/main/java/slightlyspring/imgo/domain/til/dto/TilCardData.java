cffb6bf5781f8d003ff4d42514d140ab88a40a63
gurunelee <dlckdgk4858@gmail.com>
2021-09-24 01:43:20 +0900

    feat(tilCard Json API): add dto TilCardData to be json-response

---
+++ src/main/java/slightlyspring/imgo/domain/til/dto/TilCardData.java
@@ -0,0 +1,17 @@
+package slightlyspring.imgo.domain.til.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import slightlyspring.imgo.domain.tag.domain.Tag;
+
+@Data
+@Builder
+public class TilCardData {
+  private String title;
+  private int likeCount;
+  private LocalDateTime createdAt;
+  private List<String> tags;
+  private String nickname;
+}
