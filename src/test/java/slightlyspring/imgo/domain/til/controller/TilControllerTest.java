4eeee3977640b0c0bedbe20fd086ded80ac01baa
gurunelee <dlckdgk4858@gmail.com>
2021-09-24 01:46:54 +0900

    feat(tilCard Json API): add test

--- src/test/java/slightlyspring/imgo/domain/til/controller/TilControllerTest.java
+++ src/test/java/slightlyspring/imgo/domain/til/controller/TilControllerTest.java
@@ -1,11 +1,14 @@
 package slightlyspring.imgo.domain.til.controller;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import slightlyspring.imgo.domain.til.repository.TilRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +28,7 @@ class TilControllerTest {
     private TilRepository tilRepository;
 
     @Test
+    @WithMockUser(roles = "USER")
     void write() throws Exception {
         String WRITE_API_URI = "/til/write";
 
@@ -36,13 +40,34 @@ class TilControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     void save() throws Exception {
         String SAVE_API_URI = "/til/save/";
 
         mockMvc.perform(post(SAVE_API_URI))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("user"))
-                .andExpect(view().name("/til/detail"))
-                .andDo(print());
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("user"))
+            .andExpect(view().name("/til/detail"))
+            .andDo(print());
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void getTils() throws Exception {
+        String GET_LIST_API_URI = "/til/1/til-cards";
+
+        MvcResult mvcResult = mockMvc.perform(get(GET_LIST_API_URI))
+            .andExpect(status().isOk())
+            .andReturn();
+        
+        String contentType = mvcResult.getResponse().getContentType();
+        Assertions.assertThat(contentType).contains("json");
+
+        String content = mvcResult.getResponse().getContentAsString();
+        Assertions.assertThat(content).contains("title");
+        Assertions.assertThat(content).contains("likeCount");
+        Assertions.assertThat(content).contains("createdAt");
+        Assertions.assertThat(content).contains("tags");
+        Assertions.assertThat(content).contains("nickname");
     }
 }
