28178ab90928feec2ba366777a613a1190060a50
gurunelee <dlckdgk4858@gmail.com>
2021-10-06 01:08:30 +0900

    fix : 생성자 추가

--- src/main/java/slightlyspring/imgo/domain/series/dto/SeriesCardData.java
+++ src/main/java/slightlyspring/imgo/domain/series/dto/SeriesCardData.java
@@ -1,11 +1,18 @@
 package slightlyspring.imgo.domain.series.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import java.io.Serializable;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import slightlyspring.imgo.domain.til.dto.TilCardData;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SeriesCardData {
 
   String title;
