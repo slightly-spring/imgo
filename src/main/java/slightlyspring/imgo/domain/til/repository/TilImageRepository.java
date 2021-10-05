f6729c11a59601e1f6334777624356f1b29d7e05
zwan <2zwan@naver.com>
2021-10-06 01:16:41 +0900

    feat(file-upload): add findAllByTil() to TilImageRepository

--- src/main/java/slightlyspring/imgo/domain/til/repository/TilImageRepository.java
+++ src/main/java/slightlyspring/imgo/domain/til/repository/TilImageRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import slightlyspring.imgo.domain.til.domain.Til;
 import slightlyspring.imgo.domain.til.domain.TilImage;
 
+import java.util.List;
+
 public interface TilImageRepository extends JpaRepository<TilImage, Long> {
     void deleteByTil(Til til);
+    List<TilImage> findAllByTil(Til til);
 }
