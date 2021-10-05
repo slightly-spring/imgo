2f9dad6894abf5f2b20a27a5b73684faba3f2ed8
zwan <2zwan@naver.com>
2021-10-06 01:16:55 +0900

    test(file-upload): add til save test

--- src/test/java/slightlyspring/imgo/domain/til/service/TilServiceTest.java
+++ src/test/java/slightlyspring/imgo/domain/til/service/TilServiceTest.java
@@ -12,6 +12,8 @@ import slightlyspring.imgo.domain.tag.domain.Tag;
 import slightlyspring.imgo.domain.tag.service.TagService;
 import slightlyspring.imgo.domain.til.domain.SourceType;
 import slightlyspring.imgo.domain.til.domain.Til;
+import slightlyspring.imgo.domain.til.domain.TilImage;
+import slightlyspring.imgo.domain.til.repository.TilImageRepository;
 import slightlyspring.imgo.domain.til.repository.TilRepository;
 import slightlyspring.imgo.domain.user.domain.User;
 import slightlyspring.imgo.domain.user.domain.UserTilRecord;
@@ -19,6 +21,7 @@ import slightlyspring.imgo.domain.user.repository.UserRepository;
 import slightlyspring.imgo.domain.user.repository.UserTilRecordRepository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -45,6 +48,9 @@ class TilServiceTest {
 
     @Autowired
     TagService tagService;
+
+    @Autowired
+    TilImageRepository tilImageRepository;
 
     @Test
     @Transactional
@@ -83,5 +89,65 @@ class TilServiceTest {
         assertThat(savedTil).isEqualTo(til);
         assertThat(userTilRecord.getTilCount()).isEqualTo(1);
         assertThat(userTilRecord.getCharacterCount()).isEqualTo(til.getContent().length());
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username="user")
+    void TIL_게시물_작성_시리즈태그제목없이() {
+        // given
+        User user = User.builder()
+                .nickname("member01")
+                .build();
+        User savedUser = userRepository.save(user);
+
+        Til til = Til.builder()
+                .content("til content test")
+                .sourceType(SourceType.COMMON)
+                .source("스프링 데이터 JPA")
+                .user(savedUser)
+                .build();
+
+        // when
+        Long tilId = tilService.save(til, new ArrayList<>());
+
+        // then
+        Til savedTil = tilRepository.findById(tilId).orElse(null);
+        assertThat(savedTil).isEqualTo(til);
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser(username="user")
+    void TIL_게시물_작성_사진첨부() {
+        // given
+        User user = User.builder()
+                .nickname("member01")
+                .build();
+        User savedUser = userRepository.save(user);
+
+        Series series = Series.builder()
+                .title("series title")
+                .build();
+        Series savedSeries = seriesRepository.save(series);
+
+        Til til = Til.builder()
+                .title("til title")
+                .content("<img src='abc'></img><img src='abcd'></img>")
+                .sourceType(SourceType.COMMON)
+                .source("스프링 데이터 JPA")
+                .user(savedUser)
+                .series(savedSeries)
+                .build();
+
+        // when
+        Long tilId = tilService.save(til,  new ArrayList<>());
+
+        // then
+        Til savedTil = tilRepository.findById(tilId).orElse(null);
+        List<TilImage> tilImages = tilImageRepository.findAllByTil(savedTil);
+        assertThat(savedTil).isEqualTo(til);
+        assertThat(tilImages.size()).isEqualTo(2);
+
     }
 }
