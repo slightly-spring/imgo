93a1d0e8644b18392e76cb45a079b70c3ff05789
gurunelee <dlckdgk4858@gmail.com>
2021-10-15 17:36:29 +0900

    feat(mypage/profile) : session 의 userId와, pathVariable userId 의 일치여부로 mypage/profile 분기

--- src/main/java/slightlyspring/imgo/domain/user/controller/UserController.java
+++ src/main/java/slightlyspring/imgo/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package slightlyspring.imgo.domain.user.controller;
 
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,21 +22,35 @@ import java.util.Locale;
 public class UserController {
 
     private final UserService userService;
+    private final HttpSession httpSession;
 
     @GetMapping("/{userId}")
+    // view 를 분기하지 않고, profile 에 isMyPage 속성을 넘겨서 프론트에서 분기하게끔 하기
+    // mypage 내의 읽기/작성 목록은 html 내에서 분기하도록
+    // 작성목록을 버리고 읽기목록만 개발
+    // 프로필, 작성목록 모두 상단에 '작성목록' 들어가게 하기
     public String profile(@PathVariable Long userId, Model model) {
         if (!userService.isUserExist(userId)) {
             return "404";
         }
+
+        Long sessionUserId = (Long) httpSession.getAttribute("userId");
+
         UserProfile userProfile = userService.getUserProfile(userId);
         UserProfileDetail userProfileDetail = userService.getUserProfileDetail(userId);
+
+        if (sessionUserId.equals(userId)) {
+            model.addAttribute("pageRole", "mypage");
+        } else {
+            model.addAttribute("pageRole", "profile");
+        }
 
         model.addAttribute("userProfile", userProfile)
             .addAttribute("userProfileDetail", userProfileDetail)
             .addAttribute("firstDayOfWeek",
                 LocalDate.now().with(WeekFields.of(Locale.KOREA).dayOfWeek(), 1).getDayOfMonth())
             .addAttribute("userId", userId);
-        return "/user/profile";
+        return "/user/profile_test";
     }
 
 }
