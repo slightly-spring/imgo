eed42e0720d5027de7f3cadfe2ba585e76855107
gurunelee <dlckdgk4858@gmail.com>
2021-10-25 19:57:22 +0900

    feat(til-analysis): Til save 컨트롤러 동작 시, UserService.updateLastWriteAt() 메서드 사용

--- src/main/java/slightlyspring/imgo/domain/til/controller/TilController.java
+++ src/main/java/slightlyspring/imgo/domain/til/controller/TilController.java
@@ -1,11 +1,7 @@
 package slightlyspring.imgo.domain.til.controller;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -18,13 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 import slightlyspring.imgo.domain.series.domain.Series;
 import slightlyspring.imgo.domain.series.repository.SeriesRepository;
 import slightlyspring.imgo.domain.tag.domain.Tag;
-import slightlyspring.imgo.domain.tag.repository.TagRepository;
 import slightlyspring.imgo.domain.tag.service.TagService;
 import slightlyspring.imgo.domain.til.domain.Til;
 import slightlyspring.imgo.domain.til.dto.TilForm;
 import slightlyspring.imgo.domain.til.repository.TilRepository;
 import slightlyspring.imgo.domain.til.dto.TilCardData;
-import slightlyspring.imgo.domain.til.repository.TilTagRepository;
 import slightlyspring.imgo.domain.til.service.TilCardService;
 import slightlyspring.imgo.domain.til.service.TilImageService;
 import slightlyspring.imgo.domain.til.service.TilService;
@@ -32,11 +26,7 @@ import slightlyspring.imgo.domain.user.repository.UserRepository;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
-import slightlyspring.imgo.domain.til.service.TilService;
-import slightlyspring.imgo.domain.user.repository.UserRepository;
 import slightlyspring.imgo.domain.user.service.UserService;
-import slightlyspring.imgo.infra.S3FileUploader;
-
 
 @Controller
 @RequiredArgsConstructor
@@ -46,6 +36,7 @@ public class TilController {
     private final TilService tilService;
     private final TagService tagService;
     private final UserRepository userRepository;
+    private final UserService userService;
     private final SeriesRepository seriesRepository;
     private final TilRepository tilRepository;
     private final TilImageService tilImageService;
@@ -59,16 +50,13 @@ public class TilController {
         return "/til/detail";
     }
 
-    // 테스트 데이터 생성용 repository
-    private final TagRepository tagRepository;
-    private final TilTagRepository tilTagRepository;
-
     @GetMapping("/write")
     public String write(Model model) {
         // TODO userId using principal
         Long userId = (Long) httpSession.getAttribute("userId");
 
         List<Series> seriesList = seriesRepository.findAllByUserId(userId);
+
         model.addAttribute("seriesList", seriesList);
         model.addAttribute("userId", userId);
         return "/til/write";
@@ -87,6 +75,8 @@ public class TilController {
                 .user(userRepository.getById(userId))
                 .series(seriesRepository.getById(tilForm.getSeriesId()))
                 .build();
+
+        userService.updateLastWriteAt(userId, LocalDateTime.now());
 
         List<Tag> savedTags = tagService.saveTags(tags);
         Long tilId = tilService.save(til, savedTags);
@@ -117,29 +107,4 @@ public class TilController {
     public String uploadImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
         return tilImageService.uploadImage(multipartFile, "static/til");
     }
-
-
-//    /**
-//     * 테스트용 데이터 주입
-//     */
-//    @PostConstruct
-//    public void init() {
-//        User user = User.builder().nickname("testUser").build();
-//        userRepository.save(user);
-//        int offset = 14;
-//        for (int i=0; i<offset; i++) {
-//            Tag tagA = Tag.builder().name("tag" + i).build();
-//            Tag tagB = Tag.builder().name("tag" + (i + offset)).build();
-//            tagRepository.save(tagA);
-//            tagRepository.save(tagB);
-//
-//            Til til = Til.builder().title("testTil"+i).user(user).build();
-//            tilRepository.save(til);
-//
-//            TilTag tilTagAA = new TilTag(til, tagA);
-//            TilTag tilTagAB = new TilTag(til, tagB);
-//            tilTagRepository.save(tilTagAA);
-//            tilTagRepository.save(tilTagAB);
-//        }
-//    }
 }
