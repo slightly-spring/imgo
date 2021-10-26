228f41bd03781989019f834b617db1e2dbde7af9
gurunelee <dlckdgk4858@gmail.com>
2021-10-26 18:41:06 +0900

    fix : maincontroller 추가

--- src/main/java/slightlyspring/imgo/domain/MainController.java
+++ src/main/java/slightlyspring/imgo/domain/MainController.java
@@ -1,2 +1,21 @@
-package slightlyspring.imgo.domain;public class MainController {
+package slightlyspring.imgo.domain;
+
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+  private final HttpSession httpSession;
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("userId", httpSession.getAttribute("userId"));
+    return "index";
+  }
+
 }
