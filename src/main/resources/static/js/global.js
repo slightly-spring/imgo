37fa14852b0a9ebd3c7e39df0ce9edc04d3fa256
zwan <2zwan@naver.com>
2021-10-26 21:36:58 +0900

    feat(feed): feed impl
    - fix css, js

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,4 +1,5 @@
 let isArrowBoxFold = true;
+
 function clickMypage() {
     const mypage = document.querySelector(".mypage");
     const arrowBox = document.querySelector(".arrow-box");
@@ -21,4 +22,46 @@ function closeLoginPopup() {
 function openLoginPopup() {
     const login = document.querySelector("#popup-login");
     login.style.display = "block";
+}
+
+window.onload = function () {
+    let $tilContainer = $('.til-container').infiniteScroll({
+        path: '.pagination__next',
+        // load response as JSON
+        responseBody: 'json',
+        status: '.page-load-status',
+        history: false,
+    });
+
+    $tilContainer.on('load.infiniteScroll', function (event, body) {
+        // compile body data into HTML
+        let itemsHTML = body.map(getItemHTMLFromTilCard).join('');
+        // convert HTML string into elements
+        let $items = $(itemsHTML);
+        // append item elements
+        $tilContainer.infiniteScroll('appendItems', $items);
+    });
+
+    // load initial page
+    $tilContainer.infiniteScroll('loadNextPage');
+
+
+    let $seriesContainer = $('.series-container').infiniteScroll({
+        path: '.pagination__next_series',
+        responseBody: 'json',
+        status: '.page-load-status',
+        history: false,
+    });
+
+    $seriesContainer.on('load.infiniteScroll', function (event, body) {
+        // compile body data into HTML
+        let itemsHTML = body.map(getItemHTMLFromSeriesCard).join('');
+        // convert HTML string into elements
+        let $items = $(itemsHTML);
+        // append item elements
+        $seriesContainer.infiniteScroll('appendItems', $items);
+    });
+
+    // load initial page
+    $seriesContainer.infiniteScroll('loadNextPage');
 }
