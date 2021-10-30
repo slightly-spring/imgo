eab12b44574c326053cb5d95e3ff0e00503d1acd
zwan <2zwan@naver.com>
2021-10-30 12:14:36 +0900

    feat(til-detail): impl tags, series view

--- src/main/resources/static/js/til/detail.js
+++ src/main/resources/static/js/til/detail.js
@@ -4,11 +4,6 @@ $(function() {
         $(this).parent().toggleClass('open');
     });
 
-    $('.dropdown > .list > .item').on('click', function() {
-        $('.dropdown > .list > .item').removeClass('selected');
-        $(this).addClass('selected').parent().parent().removeClass('open').children('.caption').text( $(this).text() );
-    });
-
     $(document).on('keyup', function(evt) {
         if ( (evt.keyCode || evt.which) === 27 ) {
             $('.dropdown').removeClass('open');
