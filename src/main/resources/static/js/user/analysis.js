d9e889d84f150fad9bcbb44550beff892b4f2d22
zwan <2zwan@naver.com>
2021-11-03 01:04:31 +0900

    feat(analysis): impl tagRate, myBadges

--- src/main/resources/static/js/user/analysis.js
+++ src/main/resources/static/js/user/analysis.js
@@ -11,15 +11,24 @@ cal.init({
 });
 
 
-$(function(){
-    $("#doughnutChart").drawDoughnutChart([
-        { title: "VPC",         value : 120,  color: "#156a2b" },
-        { title: "SPring", value:  80,   color: "#156a2b80" },
-        { title: "git",      value:  70,   color: "#156a2b60" },
-        { title: "Python",        value : 50,   color: "#F7E248" },
-        { title: "Java",        value : 40,   color: "#F7E24880" },
-        { title: "Javascript",        value : 20,   color: "#F7E24840" }
-    ]);
+
+$(function () {
+    const DOUGHNUT_COLOR = ["#156a2b", "#156a2b80", "#156a2b60", "#F7E248", "#F7E24880", "#F7E24840"];
+    const tagRates = document.querySelectorAll(".tagRate");
+    const chartData = [];
+    for (let i=0; i<tagRates.length; i++) {
+        chartData.push({
+            title: tagRates[i].dataset.tagRateTitle,
+            value: parseInt(tagRates[i].dataset.tagRateValue),
+            color: DOUGHNUT_COLOR[i]
+        });
+    }
+    $("#doughnutChart").drawDoughnutChart(chartData);
+});
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
 });
 /*!
  * jquery.drawDoughnutChart.js
@@ -31,28 +40,28 @@ $(function(){
  * Released under the MIT license.
  *
  */
-;(function($, undefined) {
-    $.fn.drawDoughnutChart = function(data, options) {
+;(function ($, undefined) {
+    $.fn.drawDoughnutChart = function (data, options) {
         var $this = this,
             W = $this.width(),
             H = $this.height(),
-            centerX = W/2,
-            centerY = H/2,
+            centerX = W / 2,
+            centerY = H / 2,
             cos = Math.cos,
             sin = Math.sin,
             PI = Math.PI,
             settings = $.extend({
-                segmentShowStroke : true,
-                segmentStrokeColor : "#0C1013",
-                segmentStrokeWidth : 0,
+                segmentShowStroke: true,
+                segmentStrokeColor: "#0C1013",
+                segmentStrokeWidth: 0,
                 baseColor: "rgba(0,0,0,0)",
                 baseOffset: 4,
-                edgeOffset : 10,//offset from edge of $this
-                percentageInnerCutout : 50,
-                animation : true,
-                animationSteps : 90,
-                animationEasing : "easeInOutExpo",
-                animateRotate : true,
+                edgeOffset: 10,//offset from edge of $this
+                percentageInnerCutout: 50,
+                animation: true,
+                animationSteps: 90,
+                animationEasing: "easeInOutExpo",
+                animateRotate: true,
                 tipOffsetX: -8,
                 tipOffsetY: -45,
                 tipClass: "doughnutTip",
@@ -60,27 +69,31 @@ $(function(){
                 summaryTitle: "TOTAL:",
                 summaryTitleClass: "doughnutSummaryTitle",
                 summaryNumberClass: "doughnutSummaryNumber",
-                beforeDraw: function() {  },
-                afterDrawed : function() {  },
-                onPathEnter : function(e,data) {  },
-                onPathLeave : function(e,data) {  }
+                beforeDraw: function () {
+                },
+                afterDrawed: function () {
+                },
+                onPathEnter: function (e, data) {
+                },
+                onPathLeave: function (e, data) {
+                }
             }, options),
             animationOptions = {
-                linear : function (t) {
+                linear: function (t) {
                     return t;
                 },
                 easeInOutExpo: function (t) {
-                    var v = t<.5 ? 8*t*t*t*t : 1-8*(--t)*t*t*t;
-                    return (v>1) ? 1 : v;
+                    var v = t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
+                    return (v > 1) ? 1 : v;
                 }
             },
-            requestAnimFrame = function() {
+            requestAnimFrame = function () {
                 return window.requestAnimationFrame ||
                     window.webkitRequestAnimationFrame ||
                     window.mozRequestAnimationFrame ||
                     window.oRequestAnimationFrame ||
                     window.msRequestAnimationFrame ||
-                    function(callback) {
+                    function (callback) {
                         window.setTimeout(callback, 1000 / 60);
                     };
             }();
@@ -90,7 +103,7 @@ $(function(){
         var $svg = $('<svg width="' + W + '" height="' + H + '" viewBox="0 0 ' + W + ' ' + H + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>').appendTo($this),
             $paths = [],
             easingFunction = animationOptions[settings.animationEasing],
-            doughnutRadius = Min([H / 2,W / 2]) - settings.edgeOffset,
+            doughnutRadius = Min([H / 2, W / 2]) - settings.edgeOffset,
             cutoutRadius = doughnutRadius * (settings.percentageInnerCutout / 100),
             segmentTotal = 0;
 
@@ -170,23 +183,27 @@ $(function(){
             cmd = cmd.join(' ');
             return cmd;
         };
+
         function pathMouseEnter(e) {
             var order = $(this).data().order;
             $tip.text(data[order].title + ": " + data[order].value)
                 .fadeIn(200);
-            settings.onPathEnter.apply($(this),[e,data]);
+            settings.onPathEnter.apply($(this), [e, data]);
         }
+
         function pathMouseLeave(e) {
             $tip.hide();
-            settings.onPathLeave.apply($(this),[e,data]);
+            settings.onPathLeave.apply($(this), [e, data]);
         }
+
         function pathMouseMove(e) {
             $tip.css({
                 top: e.pageY + settings.tipOffsetY,
                 left: e.pageX - $tip.width() / 2 + settings.tipOffsetX
             });
         }
-        function drawPieSegments (animationDecimal) {
+
+        function drawPieSegments(animationDecimal) {
             var startRadius = -PI / 2,//-90 degree
                 rotateAnimation = 1;
             if (settings.animation && settings.animateRotate) rotateAnimation = animationDecimal;//count up between0~1
@@ -223,19 +240,22 @@ $(function(){
                 startRadius += segmentAngle;
             }
         }
+
         function drawDoughnutText(animationDecimal, segmentTotal) {
             $summaryNumber
                 .css({opacity: animationDecimal})
                 .text((segmentTotal * animationDecimal).toFixed(1));
         }
+
         function animateFrame(cnt, drawData) {
-            var easeAdjustedAnimationPercent =(settings.animation)? CapValue(easingFunction(cnt), null, 0) : 1;
+            var easeAdjustedAnimationPercent = (settings.animation) ? CapValue(easingFunction(cnt), null, 0) : 1;
             drawData(easeAdjustedAnimationPercent);
         }
+
         function animationLoop(drawData) {
-            var animFrameAmount = (settings.animation)? 1 / CapValue(settings.animationSteps, Number.MAX_VALUE, 1) : 1,
-                cnt =(settings.animation)? 0 : 1;
-            requestAnimFrame(function() {
+            var animFrameAmount = (settings.animation) ? 1 / CapValue(settings.animationSteps, Number.MAX_VALUE, 1) : 1,
+                cnt = (settings.animation) ? 0 : 1;
+            requestAnimFrame(function () {
                 cnt += animFrameAmount;
                 animateFrame(cnt, drawData);
                 if (cnt <= 1) {
@@ -245,21 +265,26 @@ $(function(){
                 }
             });
         }
+
         function Max(arr) {
 
             return Math.max.apply(null, arr);
         }
+
         function Min(arr) {
             return Math.min.apply(null, arr);
         }
+
         function isNumber(n) {
             return !isNaN(parseFloat(n)) && isFinite(n);
         }
+
         function CapValue(valueToCap, maxValue, minValue) {
             if (isNumber(maxValue) && valueToCap > maxValue) return maxValue;
             if (isNumber(minValue) && valueToCap < minValue) return minValue;
             return valueToCap;
         }
+
         return $this;
     };
 })(jQuery);
